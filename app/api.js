f0ef7b65fb217b43c2f47881d6dafa96c426d4b5
Bernardo Soares <cbssdev@gmail.com>
2023-10-04 16:53:31 -0300

    Patch2

--- app/api.js
+++ app/api.js
@@ -1,3 +1,9 @@
-Listadeplantas => async function (){
-    fetch() 
-} 
+listaDePlantas = async () => {
+    const lista =  await fetch("https://cbssoares.github.io/pesquisador/data/plantas.json")
+    const listaAdaptada = await lista.json()
+    return(listaAdaptada)
+
+}
+
+
+
--- app/main.js
+++ app/main.js
@@ -1,24 +1,45 @@
 blocoPlantas = document.getElementById("bloco-plantas")
 pesquisaInput = document.querySelector('.pesquisa')
-Listadeplantas => async function (){
-    fetch() 
-} 
 console.log(pesquisaInput)
+
+listaDePlantas = (async () => {
+    const lista =  await fetch("https://cbssoares.github.io/pesquisador/data/plantas.json")
+    const listaAdaptada = await lista.json()
+    const listaDados = listaAdaptada.plantas
+    await listaDados.forEach( e => {
+        const bloco = document.createElement("div")
+        bloco.classList.add('divPlanta')
+        bloco.innerHTML =`<img src="" alt="">
+        <h2>Nome: ${e.Tipo}</h2>
+        <p>Pote: ${e.Pote}</p>
+        <p>Unidade: ${e.Unidade}</p>
+        <p>Valor: ${e.Preço}</p>`
+        blocoPlantas.appendChild(bloco)
+        
+    
+        })
+}
+)
+
+
+
+listaDePlantas()
+
+
+
+
 
 pesquisaInput.addEventListener('change', salvaResultado)
 
 function salvaResultado() {
     let resultado = pesquisaInput.value
-    console.log(resultado)
-    Listadeplantas
 }
 
 
 
+async function  listRender() {
+    
+}
 
-    /* <div>
-            <img src="" alt="">
-            <h2></h2>
-            <p></p>
-            <p></p>
-        </div> */
+    listRender()
+
