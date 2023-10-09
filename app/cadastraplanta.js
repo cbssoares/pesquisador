6da6edcacd529c6c8990a0fb189feac9ac3d0f58
Bernardo Soares <cbssdev@gmail.com>
2023-10-09 10:27:59 -0300

    atualiza pagina depois de cadastrar um novo item

--- app/cadastraplanta.js
+++ app/cadastraplanta.js
@@ -1,37 +1,49 @@
-import {listaDePlantas} from "./api.js"
-import {atualizaLista} from "./api.js"
+import { listaDePlantas } from "./api.js"
+import { atualizaLista } from "./api.js"
 
 const formulario = document.getElementById("formCadastraPlanta")
 
 
 
 
-formulario.addEventListener('submit',  async (e) =>{
-    cadastraPlanta(e)
+formulario.addEventListener('submit', async (e) => {
+  cadastraPlanta(e)
 })
 
 
-const cadastraPlanta =  async (form) => {
-    form.preventDefault()
-    const listaNova = await listaDePlantas()
-    console.log(await listaDePlantas())
-    const novaPlanta = {
-        imagem: form.target.imagem.value,
-        Tipo: form.target.nome.value,
-        Pote: form.target.pote.value,
-        Unidade:form.target.unidade.value,
-        Preço:form.target.preco.value
+const cadastraPlanta = async (form) => {
+  form.preventDefault()
+  let imagem = form.target.imagem.value
+  let nome = form.target.nome.value
+  let pote = form.target.pote.value
+  let unidade = form.target.unidade.value
+  let preco = form.target.preco.value
+  const listaNova = await listaDePlantas()
+  const novaPlanta = {
+    imagem: imagem,
+    Tipo: nome,
+    Pote: pote,
+    Unidade: unidade,
+    Preço: preco
+  }
+  await listaNova.push(novaPlanta)
+  listaNova.sort(function (a, b) {
+    if (a.Tipo.toLowerCase() < b.Tipo.toLowerCase()) {
+      return -1;
     }
-    await listaNova.push(novaPlanta)
-    listaNova.sort(function (a, b) {
-        if (a.Tipo.toLowerCase() < b.Tipo.toLowerCase()) {
-          return -1;
-        }
-        if (a.Tipo.toLowerCase > b.Tipo.toLowerCase) {
-          return 1;
-        }
-        return 0;
-      })
-    atualizaLista(listaNova)
-    
+    if (a.Tipo.toLowerCase > b.Tipo.toLowerCase) {
+      return 1;
+    }
+    return 0;
+  })
+  atualizaLista(listaNova)
+  imagem = ""
+  nome = ""
+  pote = ""
+  unidade = ""
+  preco = ""
+  console.log(imagem,nome,pote,unidade,preco)
+  window.alert('Item cadastrado com sucesso')
+  window.location.reload()
+
 }
