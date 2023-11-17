a851b4b72dfce1fa327b04eb1c752d1f73788903
Bernardo Soares <cbssdev@gmail.com>
2023-11-17 12:52:53 -0300

    va

--- catalogo-plantas/pages/components/app.jsx
+++ catalogo-plantas/pages/components/app.jsx
@@ -8,11 +8,42 @@ const listaP = await listaDePlantas()
 export default function App() {
 
 
+    const[lista, setlista] = useState(listaP)
+
+    const apagaItens = async (bloco) => {
+        if (window.confirm("Tem certeza que quer apagar este item ?")) {
+            console.log(bloco)
+            const item = bloco.dataset.chave 
+            listaP.splice(item, 1)
+            console.log(listaP)
+            atualizaLista(listaAtualizada)
+            setlista(listaAtualizada)
     
+        }
+    
+    }
+
+    function salvaResultado(busca) {
+        if (busca == "") {
+            setlista(listaP)
+        } else {
+            console.log(busca)
+            const listaFiltrada = listaP.filter(plantas => plantas.Tipo.toLowerCase().includes(busca.toLowerCase()))
+            console.log(listaFiltrada)
+            setlista(listaFiltrada)
+            
+            
+        }
+    
+    }
     
     return (
             <main className="principal">
-                oi
+                <div className="divPesquisa">
+                    <input className="pesquisa" type="search" placeholder="Digite o nome da planta" onKeyUp={(e) => salvaResultado(e.target.value)} />
+                    <button className="botaoPlanta" id="botaoCadastra">Cadastrar um novo item</button>
+                </div>
+                <BlocoPlantas Lista={lista} ApagaPlanta={apagaItens} ></BlocoPlantas>
             </main>
     )
 }
