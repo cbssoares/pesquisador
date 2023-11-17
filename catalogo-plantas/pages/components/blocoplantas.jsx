c516a565e74fa158260ced12e6336464553eb260
Bernardo Soares <cbssdev@gmail.com>
2023-11-17 12:08:45 -0300

    agora vai ?

--- catalogo-plantas/pages/components/blocoplantas.jsx
+++ catalogo-plantas/pages/components/blocoplantas.jsx
@@ -2,9 +2,10 @@ import ContainerPlantas from "./Container.jsx"
 // oi
 export default function BlocoPlantas(props) {
     console.log(props.Lista)
+    const BlocoLista = props.Lista
     return(
     <div id="bloco-plantas">
-        {props.Lista.map( (e) => 
+        {BlocoLista && BlocoLista.map( (e) => 
             <ContainerPlantas key = {e.id} chave = {e.id} foto = {e.imagem}  tipo = {e.Tipo} pote = {e.Pote} unidade = {e.Unidade} preco = {e.Preço} ApagaPlanta = {props.ApagaPlanta} ></ContainerPlantas>
         )}
     </div>
