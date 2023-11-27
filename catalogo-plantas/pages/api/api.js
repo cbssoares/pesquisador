bb96f9a21eff65dc05287988371ff5ff554380f0
Bernardo Soares <cbssdev@gmail.com>
2023-11-27 15:31:58 -0300

    agora vai !!

--- catalogo-plantas/pages/api/api.js
+++ catalogo-plantas/pages/api/api.js
@@ -1,7 +1,7 @@
 const listaDePlantas = (async () => {
-    const lista = await fetch("https://getpantry.cloud/apiv1/pantry/872e29e3-2574-4be6-9b55-d262402179f6/basket/plantas", { method: "GET"})
+    const lista = await fetch("https://pesquisador-seven.vercel.app/api/tempo", { method: "GET"})
     const listaAdaptada = await lista.json()
-    var listaDados = await listaAdaptada.plantas
+    var listaDados = await listaAdaptada
     console.log(await listaDados)
 
     return listaDados
