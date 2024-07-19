954eaeaa43277d3e3696e6ff420c36772dbc74db
Bernardo Soares <cbssdev@gmail.com>
2024-07-19 10:38:09 -0300

    Update api.js

--- catalogo-plantas/pages/api/api.js
+++ catalogo-plantas/pages/api/api.js
@@ -1,7 +1,7 @@
 const listaDePlantas = (async () => {
-    const lista = await fetch("", { method: "GET"})
+    const lista = await fetch("https://getpantry.cloud/apiv1/pantry/ddc41205-af29-4dbf-ada1-f7acca55d4d8/basket/plantas", { method: "GET"})
     const listaAdaptada = await lista.json()
-    var listaDados =  await listaAdaptada
+    var listaDados =  await listaAdaptada.plantas
     console.log(await listaDados)
 
     return listaDados
